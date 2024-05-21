94b5e9ea3c29ef63e4b67140750cc817439d1cec
Jeremieon <115072636+Jeremieon@users.noreply.github.com>
2024-05-21 23:50:48 +0200

    Update app.py

--- python/app.py
+++ python/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 #simple route app
 @app.route("/")
 def hello():
-    return "Hello ACGBILI Students!"
+    return "Hello BILI Students!"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8000)
